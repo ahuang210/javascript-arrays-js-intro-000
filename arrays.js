f60106c8736639a08b964d6a1426585ceb50d0c0
Ada Huang <ada10huang@gmail.com>
2020-08-10 23:16:21 +0000

    Automatically backed up by Learn

--- arrays.js
+++ arrays.js
@@ -38,5 +38,5 @@ var destructivelyRemoveElementFromEndOfArray = array => {
 }
 
 var removeElementFromEndOfArray = array => {
-  array.slice(0, array.length - (array.slice(-1)))
+  array.slice(0, array.length - (array.slice(-2)))
 }
